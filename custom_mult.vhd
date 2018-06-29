a6a570d46e15fffa115798a49b18a72cae22b7b6
lekonjak <ricardodogc@gmail.com>
2018-06-29 13:24:38 -0300

    (+) custom multiplication & custom sum

--- custom_mult.vhd
+++ custom_mult.vhd
@@ -3,15 +3,15 @@ LIBRARY ieee;
 USE ieee.numeric_std.all;
 USE ieee.std_logic_1164.all
 
-ENTITY custom IS
+ENTITY custom_mult IS
     PORT ( 
         signal dataa: in std_logic_vector(31 downto 0)
         signal datab: in std_logic_vector(31 downto 0)    -- optional
         signal result: out std_logic_vector(31 downto 0)  -- result (always required)
     );
-END ENTITY custom;
+END ENTITY custom_mult;
 
-ARCHITECTURE main OF custom IS
+ARCHITECTURE mult OF custom_mult IS
 BEGIN
-	return <= std_logic_vector(signed(dataa(31 downto 0) * signed(datab(32 downto 0));
-END main;
+	return <= std_logic_vector(signed(dataa(31 downto 0) * signed(datab(31 downto 0));
+END mult;
--- custom_sum.vhd
+++ custom_sum.vhd
@@ -3,15 +3,15 @@ LIBRARY ieee;
 USE ieee.numeric_std.all;
 USE ieee.std_logic_1164.all
 
-ENTITY custom IS
+ENTITY custom_sum IS
     PORT ( 
         signal dataa: in std_logic_vector(31 downto 0)
         signal datab: in std_logic_vector(31 downto 0)    -- optional
         signal result: out std_logic_vector(31 downto 0)  -- result (always required)
     );
-END ENTITY custom;
+END ENTITY custom_sum;
 
-ARCHITECTURE main OF custom IS
+ARCHITECTURE sum OF custom_sum IS
 BEGIN
-	return <= std_logic_vector(signed(dataa(31 downto 0) * signed(datab(32 downto 0));
-END main;
+	return <= std_logic_vector(signed(dataa(31 downto 0) + signed(datab(31 downto 0));
+END sum;
