783ea5dd5db07f127cef67cd91e9ea5c11266aa1
lekonjak <ricardodogc@gmail.com>
2018-07-02 21:39:59 -0300

    (*) fixing some vhdl syntax errors

--- custom_mult.vhd
+++ custom_mult.vhd
@@ -1,17 +1,17 @@
 LIBRARY ieee;
-
+ 
 USE ieee.numeric_std.all;
-USE ieee.std_logic_1164.all
-
+USE ieee.std_logic_1164.all;
+ 
 ENTITY custom_mult IS
-    PORT ( 
-        signal dataa: in std_logic_vector(31 downto 0)
-        signal datab: in std_logic_vector(31 downto 0)    -- optional
+    PORT (
+        signal dataa: in std_logic_vector(31 downto 0);
+        signal datab: in std_logic_vector(31 downto 0);    -- optional
         signal result: out std_logic_vector(31 downto 0)  -- result (always required)
     );
 END ENTITY custom_mult;
-
+ 
 ARCHITECTURE mult OF custom_mult IS
 BEGIN
-	return <= std_logic_vector(signed(dataa(31 downto 0) * signed(datab(31 downto 0));
-END mult;
+    result <= std_logic_vector(signed(dataa(31 downto 0)) * signed(datab(31 downto 0)));
+END ARCHITECTURE mult;
